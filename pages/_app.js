7fb4336883b8ad8123e4c13a2eff3cbb7a8cdb71
Olavo Mars <olavoamneves@gmail.com>
2023-03-30 15:16:36 -0400

    adding HeroBanner main features aswell as components in general

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import React from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+export default function App({Component, pageProps}) {
+  return <Component {...pageProps} />;
 }
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'zieh30sm',
+    dataset: 'production'
+  }
+})
