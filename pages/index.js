7fd8416cb76136598d6e1805fa46f695dc5647cb
Olavo Mars <olavoamneves@gmail.com>
2023-03-30 17:00:27 -0400

    adding Footer, navbar and product components

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,11 @@ const Home = ({products, bannerData}) => {
         <p>The future of ergonomic keyboards</p>
       </div>
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
