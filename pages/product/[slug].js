e937034bbb1eba111a1b039934d276098f0d7287
Olavo Mars <olavoamneves@gmail.com>
2023-04-18 18:27:46 -0400

    adding getStaticPaths function to correct getStaticProps bug

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,17 +2,35 @@ import React from 'react';
 import {client, urlFor} from '../../lib/client';
 
 const ProductDetails = ({product, products}) => {
+  const {image, name, details, price} = product;
   return (
     <div>
       <div className='product-detail-container'>
         <div>
           <div className='image-container'>
-            <img src='' />
+            <img src={urlFor(image && image[0])} />
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+        slug{
+            current
+        }
+    }`;
+
+  const products = await client.fetch(query);
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {paths, fallback: 'blocking'};
 };
 
 export const getStaticProps = async ({params: {slug}}) => {
